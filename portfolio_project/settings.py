dac2c0f209a2a8c5aa6fa8db89f643e154632f45
Eshukurullohdev <shukurullohergashev2@gmail.com>
2026-05-22 16:36:43 +0500

    ohirgisi

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -15,12 +15,7 @@ SECRET_KEY = 'django-insecure-*7(fp@rdq2%ufn38=6itv=ke_xmt!on!^2s#+4qsa_8c!no*#-
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "9jdj0ssy.up.railway.app",
-    "www.shukurullohdev.uz",
-    "shukurullohdev.uz",
-    "127.0.0.1",
-    "localhost",
-    "shukurullohdev.up.railway.app",
+    '*'
 ]
 
 
